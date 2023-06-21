3e6f7e913417ce11b08c06f044dc5b277980b5a2
Hemal31 <hemal311099@gmail.com>
2023-06-20 23:20:42 -0400

    Initial Commit

---
+++ Assignment2-advSwift/Question1.swift
@@ -0,0 +1,71 @@
+//
+//  Question1.swift
+//  Assignment2-advSwift
+//
+//  Created by Cambrian on 2022-10-17.
+//
+
+import Foundation
+/**
+ Question 1:
+ for this questions you need to create 3 functions
+ read the questions carefully - make you the function names parameters and return type are all the same as the question
+ 
+ FUNCTION 1:
+ create a function that will calculate the area of a square
+ function name:
+    - calculateArea
+ parameters:
+    - height: Int
+    - width: Int
+ return type:
+    - Int
+ */
+func calculateArea(height: Int,width: Int) -> Int {
+    return height * width
+   
+}
+ 
+/**
+ FUNCTION 2:
+ create a function that will convert and print a string into all caps
+ function name:
+    - allCap
+ parameters:
+    - string: String
+ return type:
+    - String
+ */
+func allCap(string: String) -> String{
+    return string.uppercased()
+}
+/**
+ FUNCTION 3:
+ create a function that will return the middle element of an array
+ function name:
+    - middle
+ paramters:
+    - array: [Int]
+ return type
+    - Int?
+ NOTE, if there is no middle element return nil. there is no middle element if the value is even
+ ex: [1,2,3,4,5,6] has no middle because 3,4 are both in the middle
+ ex: [1,2,3,4,5] has a middle of 3
+ */
+func middle(array: [Int]) -> Int? {
+    //Check if array is empty
+    if array.isEmpty {
+        return nil
+    }
+    else{
+        //calculate middle element by dividing array.count by 2
+        let middleIndex = array.count / 2
+        //check if the count of the array is even or odd
+        if array.count % 2 == 0 {
+            return nil
+        //return the middle index if its odd
+        } else {
+            return array[middleIndex]
+        }
+    }
+}
---
+++ Assignment2-advSwift/Question2.swift
@@ -0,0 +1,47 @@
+//
+//  Question2.swift
+//  Assignment2-advSwift
+//
+//  Created by Cambrian on 2022-10-17.
+//
+
+import Foundation
+/**
+ create a class with the following name, attributes, and methods
+ read the questions carefully - make you the function names parameters and return type are all the same as the question
+ 
+ 
+ Name:
+    - Rectangle
+ 
+ attributes:
+    - height: Int
+    - width: Int
+    - color: String
+ 
+ method:
+    - init(height: Int, width: Int, color: String)
+    - getPerimeter() -> Int
+    - printColor()
+    -
+ */
+class Rectangle {
+    var height: Int
+    var width: Int
+    var color: String
+   
+    init(height: Int, width: Int, color: String) {
+        self.height = height
+        self.width = width
+        self.color = color
+    }
+   
+    func getPerimeter() -> Int {
+        return 2 * (height + width)
+    }
+   
+    func printColor() {
+        print("The color of rectangle is \(color).")
+    }
+}
+
